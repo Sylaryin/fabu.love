fee78fc1d0303c1900712d415507fafb54d78460
Sylar <ys_yinqin@outlook.com>
2021-02-26 10:14:58 +0800

    Update upload.js

--- server/controllers/upload.js
+++ server/controllers/upload.js
@@ -154,7 +154,7 @@ async function parseAppAndInsertToDB(file, user, team) {
         await app.save()
         info.uploader = user.username;
         info.uploaderId = user._id;
-        info.size = fs.statSync(fileRealPath).size
+        info.size = file.size
         var version = Version(info)
         version.md5 = filemd5
         version.appId = app._id;
@@ -170,7 +170,7 @@ async function parseAppAndInsertToDB(file, user, team) {
     if (!version) {
         info.uploader = user.username;
         info.uploaderId = user._id;
-        info.size = fs.statSync(fileRealPath).size
+        info.size = file.size
         var version = Version(info)
         version.appId = app._id;
         version.md5 = filemd5
